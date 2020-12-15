c97816c3ba4e482a4ca1b6da0b9247cb2c6a01ce
Eder Lima <eder@ederlima.com.br>
2020-12-15 19:19:32 -0400

    Plugin defeituoso de cep

--- src/app/modules/cep-search/components/cep-search-form/cep-search-form.component.ts
+++ src/app/modules/cep-search/components/cep-search-form/cep-search-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-cep-search-form',
   templateUrl: './cep-search-form.component.html',
@@ -8,11 +8,13 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CepSearchFormComponent implements OnInit {
   @Output() doSearch = new EventEmitter();
   constructor() { }
+  public cep = new FormControl('', [Validators.required])
   public val = '';
   public handleSearchClick(): void {
     console.log('emitindo evento');
     this.doSearch.emit(79009250);
   }
+
   ngOnInit(): void {
   }
 
