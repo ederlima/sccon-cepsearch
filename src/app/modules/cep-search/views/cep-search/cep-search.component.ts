973d9331c45402d11c2f13e47bbdf58439bea126
Eder Lima <eder@ederlima.com.br>
2020-12-16 00:51:56 -0400

    Cancelando inscricoes de observables

--- src/app/modules/cep-search/views/cep-search/cep-search.component.ts
+++ src/app/modules/cep-search/views/cep-search/cep-search.component.ts
@@ -1,25 +1,27 @@
 import { Address } from '../../../../shared-components/interfaces/address';
 import { AddressTableComponent } from './../../../../shared-components/address-table/address-table.component';
 import { CepSearchService } from './../../services/cep-search.service';
-import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-cep-search',
   templateUrl: './cep-search.component.html',
   styleUrls: ['./cep-search.component.scss']
 })
-export class CepSearchComponent implements OnInit {
+export class CepSearchComponent implements OnInit, OnDestroy {
 
   @ViewChild('resultTable') addressList!: AddressTableComponent;
   constructor( private cepService: CepSearchService ) { }
   private searchResult!: Address;
+  private subscriptions: any[] = [];
   private doSearch(cep: number): void {
-    this.cepService.doSearch(cep).subscribe(
+    const sub = this.cepService.doSearch(cep).subscribe(
       result => {
         this.searchResult = result;
         this.addAddressToList(this.searchResult);
       }
     );
+    this.subscriptions.push(sub);
   }
   private addAddressToList(address: Address): void {
     this.addressList.addAddress(address);
@@ -31,6 +33,9 @@ export class CepSearchComponent implements OnInit {
     this.doSearch(cep);
   }
   ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
